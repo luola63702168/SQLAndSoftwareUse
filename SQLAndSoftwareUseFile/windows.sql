3ab4ddef3e85586a813b35573660aaea07425952
“luola” <ll63702168@qq.com>
2020-01-21 23:57:01 +0800

    win

--- SQLAndSoftwareUseFile/windows.sql
+++ SQLAndSoftwareUseFile/windows.sql
@@ -1,4 +1,4 @@
-shell（常见命令不做记录， 参考linux.sql）
+win shell（常见命令不做记录， 参考linux.sql）
 	-- 查看帮助信息
 	命令 ?
 	-- 查看回话
@@ -8,14 +8,16 @@ shell（常见命令不做记录， 参考linux.sql）
 	-- 在powshell下使用linux cp 即可
 
 
--- win r
--- 1、打开防火墙设置
-	wf.msc
+win r
+	-- 1、打开防火墙设置
+		wf.msc
 
--- 打开我的电脑
-win e
+打开我的电脑 win e
 
 
+Ctrl+Alt+Del - 打开任务管理器 
+
+ctrl+win+方向键 切屏
 
 
 	

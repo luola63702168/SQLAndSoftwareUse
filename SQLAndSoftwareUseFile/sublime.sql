307602736cf16b8a1be6c6f3cf83623e1fedccaa
“luola” <ll63702168@qq.com>
2020-01-31 17:58:04 +0800

    sub

--- SQLAndSoftwareUseFile/sublime.sql
+++ SQLAndSoftwareUseFile/sublime.sql
@@ -5,6 +5,7 @@
 5，ctrl d  选择光标所在的单词（连续使用选中多个）
 6.ctrl alt+光标移动 选中多块
 7.ctrl alt f 打开浏览器加载当前html文件
+8.按在alt，左右移动光标，会安装大驼峰格式进行跳动。
 
 主题使用：
 - 比较喜欢的几个配色方案:github-dark  salmononlce  juicy  humble  berrynice

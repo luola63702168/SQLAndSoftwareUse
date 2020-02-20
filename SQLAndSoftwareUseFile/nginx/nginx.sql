a65598cb0bcbfad88d3b1f2f6c9e727c4282202f
“luola” <ll63702168@qq.com>
2020-02-20 21:07:00 +0800

    up

--- SQLAndSoftwareUseFile/nginx/nginx.sql
+++ SQLAndSoftwareUseFile/nginx/nginx.sql
@@ -31,6 +31,8 @@ linux
 		- 反向代理和负载均衡的区别：前者是不同请求对应不同服务器，后者是相同请求对应不同服务器。
 
 	-- 反向代理
+		-- 作用：
+			- 路由转发，隐藏真实服务器
 		-- 案例1：
 			- 效果： 
 				- 在win下输入www.123.com 跳转到linux系统中tomcat主页面中。
@@ -55,6 +57,8 @@ linux
 				- 配置好 nginx.conf (参考：nginx.conf2 (请查询：here))
 
 	-- 负载均衡
+		-- 作用：
+			- 使服务器压力分担
 		-- 策略：
 			- 轮询（默认）：一个个来
 				    upstream myserver{
@@ -92,6 +96,31 @@ linux
 				- 启动服务器
 				- 配置好 nginx.conf (参考：nginx.conf3 (请查询：here))
 
+	--动静分离：所谓静态资源就是：html、css等静态文件，动态资源就是通过ajax或者直接发送的get、post、put、请求的数据库资源。
+		-- 方式：
+			- 静态资源和动态资源不在同一个服务器上（主流方案）
+			- 静态资源和动态资源在同一个服务器上，只是通过nginx将其分开来了。
+		-- 作用
+			- 使请求更加高效
+		-- 案例
+			- 效果：
+				- 浏览器中输入 192.168.233.12:80/html/a.html 可以访问 根目录下的/static/html/a.html
+				- 浏览器中输入 192.168.233.12:80/image/01.jpg 可以访问 根目录下的/static/image/01.jpg
+				- 实现静态资源从一个单独服务器上获取。
+			- 过程：
+				- 在linux根目录创建存储静态资源的目录: static/image/01.jpg和/static/html/a.html
+				- 配置好nginx.conf (参考：nginx.conf4 (请查询：here))
+
+	-- 补充：
+		1、location的expires参数，提高访问效率。
+			- 可通过location的expires参数设置浏览器的缓存过期时间。
+			- 在这个时间内，相同的url，会发送给nginx一个请求，然后对比服务端内容有没有修改，
+			- 如果服务器端的文件内容没有内容修改，则直接将缓存返回，状态码是304，
+			- 如果内容被修改了，则会从服务器上下载，返回的状态码是200。
+			- 这种方式很适合不经常修改的资源请求。
+		2、有关locat中的内容：
+			- autoindex on; 代表当请求这个资源的时候，会把该目录内容列举出来。
+			- root和alias:https://jingyan.baidu.com/article/3aed632edfd12e701180916b.html
 
 win
 - pass
